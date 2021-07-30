71e37aaa462112a6d7026d3b12c1486145770963
jclaudior <joaoclaudiodiva@gmail.com>
2021-07-29 21:47:03 -0300

    primeiro commit

---
+++ hr-work/src/main/java/br/com/jc/hrwork/HrWorkApplication.java
@@ -0,0 +1,13 @@
+package br.com.jc.hrwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrWorkApplication.class, args);
+    }
+
+}
---
+++ hr-work/src/test/java/br/com/jc/hrwork/HrWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jc.hrwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
