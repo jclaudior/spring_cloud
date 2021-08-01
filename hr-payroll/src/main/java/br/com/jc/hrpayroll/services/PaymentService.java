610069358ba40248c2725e3473dde4f8e182a1e0
jocribeiro <jocribeiro@rd.com.br>
2021-08-01 19:31:56 -0300

    restTemplate

--- hr-payroll/src/main/java/br/com/jc/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/jc/hrpayroll/services/PaymentService.java
@@ -4,12 +4,7 @@ import br.com.jc.hrpayroll.entities.Payment;
 import br.com.jc.hrpayroll.entities.Worker;
 import br.com.jc.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
