1c1c63d6a6e42b39b8367ba3d41b75684e8d7dab
jocribeiro <jocribeiro@rd.com.br>
2021-08-02 21:48:03 -0300

    adicionado eureca clientes

--- hr-work/src/main/java/br/com/jc/hrwork/resources/WorkerResource.java
+++ hr-work/src/main/java/br/com/jc/hrwork/resources/WorkerResource.java
@@ -2,7 +2,9 @@ package br.com.jc.hrwork.resources;
 
 import br.com.jc.hrwork.entities.Worker;
 import br.com.jc.hrwork.repositories.WorkerRepositorie;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
+    @Autowired
+    private Environment environment;
+
     @Autowired
     private WorkerRepositorie workerRepositorie;
 
@@ -25,6 +31,9 @@ public class WorkerResource {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id){
+
+        log.info("Port: " + environment.getProperty("local.server.port"));
+
         Worker worker = workerRepositorie.findById(id).get();
         return ResponseEntity.ok().body(worker);
     }
