10500c6a31b6e3e8b88a3cfd46f5f1eb4eb58eda
jocribeiro <jocribeiro@rd.com.br>
2021-08-10 08:44:31 -0300

    microsservice user

---
+++ hr-user/src/test/java/br/com/jc/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jc.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
